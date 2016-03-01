ad1127d56ad05adf60bc32c833195bacd080eeaa
Oscar Mattsson <oscar_mattsson@live.se>
2016-03-01 12:09:26 +0100

    Removed excess i2c-defs inclusions

--- settings.c
+++ settings.c
@@ -9,7 +9,6 @@
 #include <stdint.h>   /* Declarations of uint_32 and the like */
 #include <pic32mx.h>  /* Declarations of system-specific addresses etc */
 #include "chipsnake.h"  /* Declatations for game */
-#include "i2c-defs.h" /* Declarations of I2C-specific addresses */
 
 /* Initialize game logic */
 void settings_init(void) {
